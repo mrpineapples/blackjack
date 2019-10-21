10f61712ba0bd5d7f3637b4ce1b8adf43295af03
Michael Miranda <42720791+mrpineapples@users.noreply.github.com>
2019-10-21 00:52:06 -0400

    feat: accepts user input

--- main.go
+++ main.go
@@ -31,8 +31,25 @@ func main() {
 			*hand = append(*hand, card)
 		}
 	}
+	var input string
+	for input != "s" {
+		fmt.Println("Player:", player)
+		fmt.Println("Dealer:", dealer.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, cards = draw(cards)
+			player = append(player, card)
+		default:
+			if input != "s" {
+				fmt.Printf("\"%s\" is not a valid command 😢. Try again.\n\n", input)
+			}
+		}
+	}
+	fmt.Println("==FINAL HANDS==")
 	fmt.Println("Player:", player)
-	fmt.Println("Dealer:", dealer.DealerString())
+	fmt.Println("Dealer:", dealer)
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
