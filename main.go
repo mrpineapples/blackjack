177e2529b1f9c582c56ed7a568f300c8e7d303e5
Michael Miranda <42720791+mrpineapples@users.noreply.github.com>
2019-10-26 14:32:54 -0400

    chore: refactor main logic

--- main.go
+++ main.go
@@ -54,57 +54,104 @@ func min(a, b int) int {
 	return b
 }
 
-func main() {
-	// var gs GameState
-	// gs.Deck = deck.New(deck.Deck(3), deck.Shuffle)
+func Shuffle(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Deck = deck.New(deck.Deck(3), deck.Shuffle)
+	return ret
+}
 
-	cards := deck.New(deck.Deck(3), deck.Shuffle)
+func Deal(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Player = make(Hand, 0, 5)
+	ret.Dealer = make(Hand, 0, 5)
 	var card deck.Card
-	var player, dealer Hand
 	for i := 0; i < 2; i++ {
-		for _, hand := range []*Hand{&player, &dealer} {
-			card, cards = draw(cards)
-			*hand = append(*hand, card)
-		}
+		card, ret.Deck = draw(ret.Deck)
+		ret.Player = append(ret.Player, card)
+		card, ret.Deck = draw(ret.Deck)
+		ret.Dealer = append(ret.Dealer, card)
 	}
+	ret.State = StatePlayerTurn
+	return ret
+}
 
-	var input string
-	for input != "s" {
-		fmt.Println("Player:", player)
-		fmt.Println("Dealer:", dealer.DealerString())
-		fmt.Println("What will you do? (h)it, (s)tand")
-		fmt.Scanf("%s\n", &input)
-		switch input {
-		case "h":
-			card, cards = draw(cards)
-			player = append(player, card)
-		default:
-			if input != "s" {
-				fmt.Printf("\"%s\" is not a valid command 🤕. Try again.\n\n", input)
-			}
-		}
+func Hit(gs GameState) GameState {
+	ret := clone(gs)
+	hand := ret.CurrentPlayer()
+	var card deck.Card
+	card, ret.Deck = draw(ret.Deck)
+	*hand = append(*hand, card)
+	if hand.Score() > 21 {
+		Stand(ret)
 	}
+	return ret
+}
 
-	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
-		card, cards = draw(cards)
-		dealer = append(dealer, card)
-	}
+func Stand(gs GameState) GameState {
+	ret := clone(gs)
+	ret.State++
+	return ret
+}
 
-	pScore, dScore := player.Score(), dealer.Score()
+func EndHand(gs GameState) GameState {
+	ret := clone(gs)
+	pScore, dScore := ret.Player.Score(), ret.Dealer.Score()
 	fmt.Println("==FINAL HANDS==")
-	fmt.Println("Player:", player, "\nScore:", pScore)
-	fmt.Println("Dealer:", dealer, "\nScore:", dScore)
+	fmt.Println("Player:", ret.Player, "\nScore:", pScore)
+	fmt.Println("Dealer:", ret.Dealer, "\nScore:", dScore)
 	switch {
 	case pScore > 21:
-		fmt.Println("You busted 😢")
+		fmt.Println("You busted! You lose 😢")
 	case dScore > 21:
-		fmt.Println("Dealer busted 🎉")
+		fmt.Println("Dealer busted! You win 🎉")
 	case pScore > dScore:
 		fmt.Println("You win 🎉")
 	case dScore > pScore:
 		fmt.Println("You lose 😢")
 	case dScore == pScore:
 		fmt.Println("Draw!")
+	}
+	fmt.Println()
+	ret.Player = nil
+	ret.Dealer = nil
+	return ret
+}
+
+func main() {
+	var gs GameState
+	gs = Shuffle(gs)
+
+	// Play 5 games of blackjack
+	for i := 0; i < 5; i++ {
+		gs = Deal(gs)
+
+		var input string
+		for gs.State == StatePlayerTurn {
+			fmt.Println("Player:", gs.Player)
+			fmt.Println("Dealer:", gs.Dealer.DealerString())
+			fmt.Println("What will you do? (h)it, (s)tand")
+			fmt.Scanf("%s\n", &input)
+			switch input {
+			case "h":
+				gs = Hit(gs)
+			case "s":
+				gs = Stand(gs)
+			default:
+				if input != "s" {
+					fmt.Printf("\"%s\" is not a valid option 🤕. Try again.\n\n", input)
+				}
+			}
+		}
+
+		for gs.State == StateDealerTurn {
+			if gs.Dealer.Score() <= 16 || (gs.Dealer.Score() == 17 && gs.Dealer.MinScore() != 17) {
+				gs = Hit(gs)
+			} else {
+				gs = Stand(gs)
+			}
+		}
+
+		gs = EndHand(gs)
 	}
 }
 
